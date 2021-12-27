beef3ec4e3ccf4440aa9eefc8796c09ebb63d255
shaheryar Haider <shaheryarjerry@gmail.com>
2021-12-28 00:18:50 +0500

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([
+    { text: "task 1", id: 1 },
+    { text: "task 2", id: 2 },
+  ]);
+
+  const [value, setValue] = useState("");
+  const [index, setIndex] = useState(0);
+  const [editStart, setEditStart] = useState(false);
+
+  const handleAddValue = () => {
+    if (value) {
+      setData([...data, { id: Math.random(), text: value }]);
+      setValue("");
+    }
+  };
+
+  const handleEditStart = (data, index) => {
+    setEditStart(true);
+    setValue(data.text);
+    setIndex(index);
+  };
+
+  const handleEdit = () => {
+    data.splice(index, 1, { id: Math.random(), text: value });
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div >
+        {data?.map((e, index) => (
+          <div key={e.id} >
+            <h2>{e.text}</h2>
+            <button onClick={() => handleEditStart(e, index)}>Edit</button>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        {editStart ? (
+          <button onClick={handleEdit}>Edit</button>
+        ) : (
+          <button onClick={handleAddValue}>Add</button>
+        )}
+      </div>
     </div>
   );
 }
